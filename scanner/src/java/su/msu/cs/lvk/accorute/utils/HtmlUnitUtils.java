9054cfee5297b559662bb7a3bbe84ae5a77e1198
ngo <ngo@c0ed8891-d990-4b3b-bf5b-3f27487f2039>
2011-12-18 22:30:51 +0000

    HtmlPage clonning: experimental implementation using the protected HtmlPage.clone()
    Thanks to introspection for this trick
    
    Note: this still results in a bunch of requests for embedded js on clone, need to workaround that.

--- scanner/src/java/su/msu/cs/lvk/accorute/utils/HtmlUnitUtils.java
+++ scanner/src/java/su/msu/cs/lvk/accorute/utils/HtmlUnitUtils.java
@@ -1,9 +1,12 @@
 package su.msu.cs.lvk.accorute.utils;
 
+import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.WebWindow;
 import com.gargoylesoftware.htmlunit.html.*;
 import org.w3c.dom.NamedNodeMap;
 
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.*;
 
 /**
@@ -62,52 +65,59 @@ public class HtmlUnitUtils {
         return controllableFieldNames;
     }
     public static HtmlPage clonePage(HtmlPage other, WebWindow w){
-        HtmlPage p = new HtmlPage(other.getUrl(), other.getWebResponse(), w);
-        p.setEnclosingWindow(w);
-        w.setEnclosedPage(p);
-        DomNode childNode = other.getFirstChild();
-        while(childNode != null){
-            DomNode newContents = childNode.cloneNode(true);
-            p.appendChild(newContents);
-            childNode = childNode.getNextSibling();
-        }
-        List<HtmlElement> scripts = p.getElementsByTagName("script");
-        p.getDocumentElement();
-        for(HtmlElement sc: scripts){
-            HtmlScript script = (HtmlScript)sc;
-            DomNode n = script.getParentNode();
-            HtmlScript newScript = (HtmlScript) p.createElement("script");
-            NamedNodeMap map =  script.getAttributes();
-            for(int i=0;i<map.getLength();i++){
-                newScript.setAttribute(map.item(i).getNodeName(),map.item(i).getNodeValue());
+        try{
+            Method cl = other.getClass().getDeclaredMethod("clone",null);
+            cl.setAccessible(true);
+            HtmlPage p = (HtmlPage)cl.invoke(other,null);
+            return p;
+        }catch(Exception ex){
+            HtmlPage p = new HtmlPage(other.getUrl(), other.getWebResponse(), w);
+            p.setEnclosingWindow(w);
+            w.setEnclosedPage(p);
+            DomNode childNode = other.getFirstChild();
+            while(childNode != null){
+                DomNode newContents = childNode.cloneNode(true);
+                p.appendChild(newContents);
+                childNode = childNode.getNextSibling();
             }
-            newScript.setTextContent(script.getTextContent());
-            newScript.setNodeValue(script.getNodeValue());
-            n.removeChild(script);
-            n.appendChild(newScript);
-        }
-        List<HtmlElement> cssLinks = p.getElementsByTagName("link");
-        for(HtmlElement l: cssLinks){
-            HtmlLink link = (HtmlLink)l;
-            DomNode n = link.getParentNode();
-            HtmlLink newLink = (HtmlLink) p.createElement("link");
-            NamedNodeMap map =  link.getAttributes();
-            for(int i=0;i<map.getLength();i++){
-                newLink.setAttribute(map.item(i).getNodeName(),map.item(i).getNodeValue());
+            List<HtmlElement> scripts = p.getElementsByTagName("script");
+            p.getDocumentElement();
+            for(HtmlElement sc: scripts){
+                HtmlScript script = (HtmlScript)sc;
+                DomNode n = script.getParentNode();
+                HtmlScript newScript = (HtmlScript) p.createElement("script");
+                NamedNodeMap map =  script.getAttributes();
+                for(int i=0;i<map.getLength();i++){
+                    newScript.setAttribute(map.item(i).getNodeName(),map.item(i).getNodeValue());
+                }
+                newScript.setTextContent(script.getTextContent());
+                newScript.setNodeValue(script.getNodeValue());
+                n.removeChild(script);
+                n.appendChild(newScript);
             }
-            n.removeChild(link);
-            n.appendChild(newLink);
+            List<HtmlElement> cssLinks = p.getElementsByTagName("link");
+            for(HtmlElement l: cssLinks){
+                HtmlLink link = (HtmlLink)l;
+                DomNode n = link.getParentNode();
+                HtmlLink newLink = (HtmlLink) p.createElement("link");
+                NamedNodeMap map =  link.getAttributes();
+                for(int i=0;i<map.getLength();i++){
+                    newLink.setAttribute(map.item(i).getNodeName(),map.item(i).getNodeValue());
+                }
+                n.removeChild(link);
+                n.appendChild(newLink);
 
+            }
+            /*Map<Object, Object> repl = new HashMap<Object, Object>();
+            repl.put(other.getEnclosingWindow(), w);
+            Window newWin = (Window)  SerialClone.clone(((Window)other.getEnclosingWindow().getScriptObject()),repl);
+            w.setScriptObject(newWin);*/
+            if(other.getFocusedElement()!=null){
+                String focusPath = other.getFocusedElement().getCanonicalXPath();
+                p.setFocusedElement(p.<HtmlElement>getFirstByXPath(focusPath));
+            }
+            return p;
         }
-        /*Map<Object, Object> repl = new HashMap<Object, Object>();
-        repl.put(other.getEnclosingWindow(), w);
-        Window newWin = (Window)  SerialClone.clone(((Window)other.getEnclosingWindow().getScriptObject()),repl);
-        w.setScriptObject(newWin);*/
-        if(other.getFocusedElement()!=null){
-            String focusPath = other.getFocusedElement().getCanonicalXPath();
-            p.setFocusedElement(p.<HtmlElement>getFirstByXPath(focusPath));
-        }
-        return p;
     }
 }
 
